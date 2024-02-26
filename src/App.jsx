108d3e31ba59bbd226d410177c30f11b4d15658d
DRV111 <xblackcrownx@gmail.com>
2024-02-26 15:16:52 +0300

    feat: add global styles with styled-components library

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Input from './ui/Input';
+import Button from './ui/Button';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>App</H1>
+        <Button>Click me</Button>
+        <Input type="text" placeholder="Start typing here..." />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;

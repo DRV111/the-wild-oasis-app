9bd503abdc8b0ea2b8dc54886f0f8dd83aca0f5d
DRV111 <xblackcrownx@gmail.com>
2024-03-02 16:02:35 +0300

    feat: add some styles to Header component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,13 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  background-color: green;
+  padding: 1.2rem 4.8rem;
+  bottom: 1px solid var(--color-grey-100);
+`;
+
 function Header() {
-  return <header>Header</header>;
+  return <StyledHeader>Header</StyledHeader>;
 }
 
 export default Header;

93ac21652a736c565b4dd29e9540a254f8632143
DRV111 <xblackcrownx@gmail.com>
2024-03-02 16:20:09 +0300

    refactor: minor style changes

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const StyledHeader = styled.header`
-  background-color: green;
+  background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   bottom: 1px solid var(--color-grey-100);
 `;

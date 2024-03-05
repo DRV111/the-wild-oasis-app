969f1b8826c88e77fd9dfa8ec0d74999aced6c12
DRV111 <xblackcrownx@gmail.com>
2024-03-05 14:54:43 +0300

    feat: setup supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://ruygedwpzsjdngexyrih.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1eWdlZHdwenNqZG5nZXh5cmloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2MjY3OTUsImV4cCI6MjAyNTIwMjc5NX0.cPx_hBBtH0HFGq3YMcZNAlewA-Lz5kD-00DMmCi-pDU';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;

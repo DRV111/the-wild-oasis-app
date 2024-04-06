e13eba71e90d2cbff0334082717544b2ab519097
DRV111 <xblackcrownx@gmail.com>
2024-04-06 13:40:13 +0300

    feat: add booking filter feature

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,26 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  //Filter
+
+  const filteredValue = searchParams.get('status');
+
+  const filter =
+    !filteredValue || filteredValue === 'all'
+      ? null
+      : { field: 'status', value: filteredValue };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return {
